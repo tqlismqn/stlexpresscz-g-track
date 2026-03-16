3e64e206163b30429a17ea1544f695bbf1d673b9
thomas.gradinar <thomas.gradinar@icloud.com>
2026-03-16 16:13:12 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Settings from '@/pages/Settings';
 
 const AuthenticatedApp = () => {
   const { isLoadingAuth, isLoadingPublicSettings, authError, navigateToLogin } = useAuth();
+  useAppInitialization();
 
   // Show loading spinner while checking app public settings or auth
   if (isLoadingPublicSettings || isLoadingAuth) {
